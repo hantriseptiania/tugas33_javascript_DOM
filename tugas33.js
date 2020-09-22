c6cfda46c915f75f03d49e3b95acdd2b10b110e9
hantriseptiania <71365751+hantriseptiania@users.noreply.github.com>
2020-09-22 09:42:41 +0700

    Add files via upload

---
+++ tugas33.js
@@ -0,0 +1,10 @@
+function panggilEksternal(){
+	var namadepan = document.getElementByID('namadepan')
+	var namabelakang = document.getElementByID('namabelakang')
+	var hobi = document.getElementByID ('hobi')
+	var forminput = document.getElementsByClassName('forminput')
+	var tagname = document.getElementsByTagName("input")
+
+	console.log(forminput)
+	console.log(tagname)
+}
